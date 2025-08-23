839b8dc388dc1a667444504b5cb3d30ad454cd26
Patch011 <crimsonred011@gmail.com>
2025-08-23 18:12:14 +0800

    MovedScript
    Moved RoomScript to RoomGenerator
    
    seperated Room Generation
    and Room Placement

--- Assets/Scripts/Room Scripts/GlobalRoomData.cs
+++ Assets/Scripts/Room Scripts/GlobalRoomData.cs
@@ -6,13 +6,17 @@ public class GlobalRoomData
 {
     public static int RoomIDincr = 0;
     public static List<string> RoomID = new List<string>();
-   
-  
+
+
+    public static GameObject NextRoom;
 
     //Room Positions and Rotations
     public static Dictionary<string, Vector3> RoomPositions = new Dictionary<string, Vector3>();
     public static Dictionary<string, Quaternion> RoomRotations = new Dictionary<string, Quaternion>();
     public static int Range = 50; // Doesnt Count First Room "Room Generator"
 
+  
 
+
+  
 }
--- Assets/Scripts/Room Scripts/RoomGenerator.cs
+++ Assets/Scripts/Room Scripts/RoomGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
@@ -6,12 +7,103 @@ public class RoomGenerator : MonoBehaviour
     public GameObject RoomTemplateRight;
     public GameObject RoomTemplateForward;
     public GameObject RoomTemplateLeftAndRight;
-   
+    public void GenerateRoomPositions()
+    {
+        Vector3 currentRoomPos = transform.position; //Seed
+        Quaternion currentRoomRot = transform.rotation; //Current Room Rotation take forward direction from here
+        float roomLength = RoomTemplate.GetComponent<BoxCollider>().size.z;
+        float roomWidth = RoomTemplate.GetComponent<BoxCollider>().size.x;
+
+        while (GlobalRoomData.RoomID.Count < GlobalRoomData.Range)
+        {
+
+            //Room Index vvvv
+            GlobalRoomData.RoomIDincr++;
+            GlobalRoomData.RoomID.Add("Room" + GlobalRoomData.RoomIDincr);
+            Debug.Log("Room IDS Made:" + GlobalRoomData.RoomIDincr);
+     
+
+            List<Vector3> Directions = new List<Vector3> { currentRoomPos + RoomTemplate.transform.forward * roomLength, currentRoomPos + RoomTemplate.transform.right * roomLength, currentRoomPos - RoomTemplate.transform.right * roomLength };
+
+            Debug.Log("Added" + GlobalRoomData.RoomID[GlobalRoomData.RoomIDincr - 1] + " position is: " + currentRoomPos + " rotation is: " + currentRoomRot);
+
+            //Adds respective RoomID and their Global positions into a dict
+            GlobalRoomData.RoomRotations.Add("Room" + GlobalRoomData.RoomIDincr, currentRoomRot);
+            GlobalRoomData.RoomPositions.Add("Room" + GlobalRoomData.RoomIDincr, currentRoomPos);
+
+            Debug.Log("Rooms in Dictionary RoomPositions:" + GlobalRoomData.RoomPositions.Count);
+
+            //values for next room       ---------------- NEXT ROOM DETAILS -------------------------------
+
+            float threshold = 0.01f;     //Allowable for short distance
+            int i = 0;
+            while (i < Directions.Count)
+            {
+                bool occupied = false;
+                foreach (Vector3 pos in GlobalRoomData.RoomPositions.Values)
+                {
+
+                    if (Vector3.Distance(pos, Directions[i]) < threshold)    //checks position of any room currently placed and compares distance between that and positions for directions to remove them from list and avoid collision
+                    {
+                        occupied = true;
+                        break;
+                    }
+                }
+
+                if (occupied)
+                {
+                    Directions.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            if (Directions.Count > 0)
+            {
+                Vector3 spawnPos = Directions[Random.Range(0, Directions.Count)];
+                currentRoomPos = spawnPos;
+                
+
+            }
+            else
+            {
+                Debug.LogWarning("No available positions to spawn the room!");
+            }
+
+
+        }
+        PlaceRoom();
+
+
+
+
+
+
+    }
+    public void PlaceRoom()
+    {
+
+        // values for THIS room  ---------------- CURRENT ROOM DETAILS ------------------------
+
+        foreach (Vector3 Room in GlobalRoomData.RoomPositions.Values)
+        {
+            Instantiate(RoomTemplate, Room, Quaternion.identity);
+        }
+      
+      
+
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector3 GeneratorPos = transform.position;
-        Instantiate(RoomTemplate,GeneratorPos,Quaternion.identity);
+      
+      
+        GenerateRoomPositions();
+  
     }
 
     // Update is called once per frame
